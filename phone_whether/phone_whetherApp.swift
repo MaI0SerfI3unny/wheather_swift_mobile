2f4dadf6a7ba99c9095f75cc52e0da4268e7a679
daniledkin <daniledkin@gmail.com>
2024-05-06 18:42:00 +0300

    Initial Commit

---
+++ phone_whether/phone_whetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  phone_whetherApp.swift
+//  phone_whether
+//
+//  Created by Danil Edkin on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct phone_whetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
